0cfe81d4b31a50e520dbacc650950dc6e370d075
Philippe Weitz <philippe.weitz@ki.se>
2019-07-31 16:29:33 +0200

    Update bots.py

--- bots.py
+++ bots.py
@@ -6,9 +6,8 @@ from telegram import Bot
 class BaseBot(Callback):
     """
     Base class for tf/keras bot callback.
-    Bots should inherit from this class
-    and implement their own _send_message
-    method. 
+    Use this callback to send out live updates
+    of model training.
     """
     def __init__(self, freq=1, init_message=None):
         """
@@ -24,6 +23,7 @@ class BaseBot(Callback):
         self.freq = freq
         self.init_message = init_message
         self.epoch = None
+        self.logs = dict()
 
     def on_train_begin(self, logs={}):
         """
@@ -36,20 +36,21 @@ class BaseBot(Callback):
         """
         Send a message at end of training.
         """
-        message = self._generate_message(self.epoch, logs)
+        message = self._generate_message()
         self._send_message(message)
 
     def on_epoch_end(self, epoch, logs={}):
         """
         Send a message every freq epochs.
         If freq is None, no messages are sent
-        from here but only from on_train_end. 
+        from here.
         """
         self.epoch = epoch
+        self.logs = logs
         if self.freq is None:
             return
         elif epoch % self.freq == 0:
-            message = self._generate_message(epoch, logs)
+            message = self._generate_message()
             self._send_message(message)
 
     def _send_message(self, message):
@@ -59,8 +60,7 @@ class BaseBot(Callback):
         """
         raise NotImplementedError('Implement in subclass!')
 
-    @staticmethod
-    def _generate_message(epoch, logs):
+    def _generate_message(self):
         """
         Generate the message to be send
         based on current log and epoch.
@@ -80,55 +80,18 @@ class BaseBot(Callback):
         :param logs: (dict)
         :return: (str)
         """
-        message = 'epoch: ' + str(epoch) + '\n'
-        for key in logs:
+        message = 'epoch: ' + str(self.epoch) + '\n'
+        for key in self.logs:
             if key == 'lr':
-                number = '{:.2E}'.format(logs[key])
+                number = '{:.2E}'.format(self.logs[key])
             else:
-                number = '{0:.4f}'.format(logs[key])
+                number = '{0:.4f}'.format(self.logs[key])
             line = key \
                    + ': ' \
                    + number \
                    + '\n'
             message += line
         return message
-
-
-class TwitterBot(BaseBot):
-    """
-    Tweet messages live during training.
-    """
-    def __init__(self, consumer_key,
-                 consumer_secret,
-                 access_token,
-                 access_token_secret,
-                 freq=1, init_message=None):
-        """
-        For details on how to obtain twitter API
-        tokens, please refer to readme.
-
-        :param consumer_key: (str)
-        :param consumer_secret: (str)
-        :param access_token: (str)
-        :param access_token_secret: (str)
-        :param freq: (int) or None
-        :param init_message: (str) or None
-        """
-        self.twitter = Twython(
-            consumer_key,
-            consumer_secret,
-            access_token,
-            access_token_secret)
-
-        super().__init__(freq=freq,
-                         init_message=init_message)
-
-    def _send_message(self, message):
-        """
-        Send out tweet.
-        :param message: (str)
-        """
-        self.twitter.update_status(status=message)
 
 
 class TelegramBot(BaseBot):
